2d7e71d6d85db2a563e95dde427b1aa07dda2317
승환 윤 <safy1591@naver.com>
2022-09-06 19:38:07 +0900

    문자열 함수 구현해보기

--- HonGong/string.c
+++ HonGong/string.c
@@ -1,33 +1,229 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <string.h>
 
 
 
-/* strcpy 备泅*/
-char* strcpy_2(char* dest, const char* src);
+/* ============================================= strcpy 备泅*/
+//char* my_strcpy(char* dest, const char* src);
+//
+//void main() {
+//	char dest1[] = "dest";
+//	char dest2[] = "dest";
+//	char* dest3 = "dest";
+//	char* dest3_c = calloc(5, sizeof(char));
+//
+//
+//	strcpy(dest1, "src");
+//	printf("strcpy func ------> %s\n", dest1);
+//
+//	my_strcpy(dest2, "src");
+//	printf("make func --------> %s\n", dest2);
+//
+//	my_strcpy(dest3_c, dest3);
+//	printf("make func --------> %s\n", dest3_c);
+//
+//	free(dest3_c);
+//}
 
-void main() {
-	char dest1[] = "dest";
-	char dest2[] = "dest";
+char* my_strcpy(char* dest, const char* src) {
+	while (*src != '\0') {
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
 
-	strcpy(dest1, "src");
-	printf("strcpy func ------> %s\n", dest1);
-
-
-	strcpy_2(dest2, "src");
-	printf("make func --------> %s\n", dest2);
-
+	return dest;
 }
 
-char* strcpy_2(char* dest, const char* src) {
-	char* str = dest;
+/* ========================================================= strncpy 备泅 */
+//char* my_strncpy(char* dest, const char* src, int n);
+//
+//void main() {
+//	char dest1[] = "dest";
+//	char dest2[] = "dest";
+//	char* dest3 = "dest";
+//	char* dest3_c = calloc(5,sizeof(char));
+//
+//	strncpy(dest1, "src", 2);
+//	printf("strncpy func ------> %s\n", dest1);
+//
+//	my_strncpy(dest2, "src", 2);
+//	printf("make func --------> %s\n", dest2);
+//
+//	my_strncpy(dest3_c, dest3,2);
+//	printf("make func --------> %s\n", dest3_c);
+//	free(dest3_c);
+//}
+//
+char* my_strncpy(char* dest, const char* src, int n) {
 
-	while (*src != NULL) {
-		*str++ = *src++;
+		for (int i = 0; i < n; i++) {
+			*dest = *src;
+			dest++;
+			src++;
+		}
+
+	return dest;
+}
+
+/* ======================================================= strcat 备泅*/
+//char* my_strcat(char* dest, const char* src);
+//
+//void main() {
+//	char dest1[10] = "dest";
+//	char dest2[10] = "dest";
+//	char* dest3 = "dest";
+//	char* dest3_c = calloc(10, sizeof(char));
+//	
+//	strcat(dest1, "src");
+//	printf("strcat func ------> %s\n", dest1);
+//
+//	my_strcat(dest2, "src");
+//	printf("my_strcat func ------> %s\n", dest2);
+//
+//	strcpy(dest3_c, dest3);
+//	my_strcat(dest3_c, dest3);
+//	printf("make func --------> %s\n", dest3_c);
+//
+//	free(dest3_c);
+//}
+
+char* my_strcat(char* dest, const char* src) {
+	while (*dest != '\0') {
+		dest++;
 	}
-	*str = '\0';
+	while (*src != '\0') {
+		*dest = *src;
+		dest++;
+		src++;
+	}
 
-	return str;
+	*dest = '\0';
+
+	return dest;
+}
+
+/* ======================================================= strncat 备泅*/
+//char* my_strncat(char* dest, const char* src, int n);
+//
+//void main() {
+//	char dest1[10] = "dest";
+//	char dest2[10] = "dest";
+//	char* dest3 = "dest";
+//	char* dest3_c = calloc(10, sizeof(char));
+//	
+//	strncat(dest1, "src", 2);
+//	printf("strncat func ------> %s\n", dest1);
+//
+//	my_strncat(dest2, "src", 2);
+//	printf("my_strncat func ------> %s\n", dest2);
+//
+//	strcpy(dest3_c, dest3);
+//	my_strncat(dest3_c, dest3, 2);
+//	printf("my_strncat func ------> %s\n", dest3_c);
+//
+//	free(dest3_c);
+//
+//}
+
+char* my_strncat(char* dest, const char* src, int n) {
+	
+	while (*dest != '\0') {
+		dest++;
+	}
+	for (int i = 0; i < n; i++) {
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	return dest;
+}
+
+/* ======================================================= strcmp 备泅*/
+//int my_strcmp(char* dest, const char* src);
+//
+//void main() {
+//		char dest1[] = "dest";
+//		char dest2[] = "dest";
+//		int res = 0;
+//
+//		res = strcmp(dest1, "dest");
+//		printf("strcmp func(true) ------> %d\n", res);
+//
+//		res = strcmp(dest1, "asrc");
+//		printf("strcmp func(false) dest > src ------> %d\n", res);
+//
+//		res = strcmp(dest1, "dsrc");
+//		printf("strcmp func(false) dest < src ------> %d\n", res);
+//
+//		res = my_strcmp(dest2, "dest");
+//		printf("my_strcmp func(true) ------> %d\n", res);
+//
+//		res = my_strcmp(dest2, "asrc");
+//		printf("my_strcmp func(false) dest > src ------> %d\n", res);
+//
+//		res = my_strcmp(dest2, "dsrc");
+//		printf("my_strcmp func(false) dest < src ------> %d\n", res);
+//		
+//}
+
+int my_strcmp(char* dest, const char* src) {
+
+	while (*dest != '\0') {
+		if (*dest == *src) {
+			dest++;
+			src++;
+		}
+		if (*dest != *src) {
+			if (*dest > *src) return  1;
+			if (*dest < *src) return  -1;
+		}
+	}
+	if (*dest == '\0')
+		return 0;
+}
+
+/* ======================================================= strncmp 备泅*/
+//int my_strncmp(char* dest, const char* src,int n);
+//void main() {
+//		char dest1[] = "dest";
+//		char dest2[] = "dest";
+//		int res = 0;
+//
+//		res = strncmp(dest1, "dest", 2);
+//		printf("strncmp func(true) ------> %d\n", res);
+//
+//		res = strncmp(dest1, "asrc", 2);
+//		printf("strncmp func(false) dest > src ------> %d\n", res);
+//
+//		res = strncmp(dest1, "dsrc", 2);
+//		printf("strncmp func(false) dest < src ------> %d\n", res);
+//
+//		res = my_strncmp(dest2, "dest", 2);
+//		printf("my_strncmp func(true) ------> %d\n", res);
+//
+//		res = my_strncmp(dest2, "asrc", 2);
+//		printf("my_strncmp func(false) dest > src ------> %d\n", res);
+//
+//		res = my_strncmp(dest2, "dsrc", 2);
+//		printf("my_strncmp func(false) dest < src ------> %d\n", res);
+//}
+
+int my_strncmp(char* dest, const char* src, int n) {
+
+	for (int i = 0; i < n; i++) {
+		if (*dest == *src) {
+			dest++;
+			src++;
+		}
+		
+		if (*dest != *src) {
+			if (*dest > *src) return 1;
+			if (*dest < *src) return -1;
+		}
+	}
+	return 0;
 }
--- HonGong/프로그래머스.c
+++ HonGong/프로그래머스.c
@@ -99,70 +99,121 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <string.h>
 
-// id_list_len은 배열 id_list의 길이입니다.
-// report_len은 배열 report의 길이입니다.
-// 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
-int* solution(const char* id_list[], size_t id_list_len, const char* report[], size_t report_len, int k) {
-	// return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
-	int* answer = (int*)malloc(sizeof(int) * id_list_len);
-	char report_cpy[][22] = { 0, };
-	int count = 0;
-
-	for (int i = 0; i < report_len ; i++) { // 배열 복사
-		strcpy(report_cpy[i], report[i]);
-	}
-
-
-	for (int i = 0; i < report_len; i++) { // report_cpy에 요소가 중첩인 경우 중첩인 요소 null로 변경
-		for (int j = 0; j < report_len - 1; j++) {
-			if (report[i] == report[j + 1])
-				strcpy(report_cpy[j + 1], NULL);
-		}
-	}
-
-	for (int i = 0; i < id_list_len; i++)
-	{
-		if (strchr(report_cpy[i], ' ') != NULL) {
-			
-		}
-
-		for (int j = 0; j < id_list_len - 1; j++)
-		{
-			if (strcmp(report[i], id_list[j]) == 0)
-				count++;
-		}			
-
-	}
-
-
-
-	return answer;
-}
-
-//int main() {
-//	char id_list[][11] = { "muzi","frodo","apeach","neo" };
-//	char report[][21] = { "muzi frodo", "apeach frodo", "frodo neo", "muzi neo", "apeach muzi" };
-//	int k = 0;
-//	int len = strlen(id_list);
-//	int re_len = strlen(report);
-//	int* res = (int*)malloc(sizeof(int) * len);
+//// id_list_len은 배열 id_list의 길이입니다.
+//// report_len은 배열 report의 길이입니다.
+//// 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
+//int* solution(const char* id_list[], size_t id_list_len, const char* report[], size_t report_len, int k) {
+//	// return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
+//	int* answer = (int*)malloc(sizeof(int) * id_list_len);
+//	char report_cpy[][22] = { 0, };
+//	int count = 0;
 //
-//	char report_cpy[][21] = report;
+//	for (int i = 0; i < report_len ; i++) { // 배열 복사
+//		strcpy(report_cpy[i], report[i]);
+//	}
 //
-//	//for (int i = 0; i < 5; i++) {
-//	//	strcpy(report_cpy[i], report[i]);
-//	//}
 //
-//	int i = 0;
-//
-//	for (i = 0; i < 5; i++) {
-//		for (int j = 0; j < 5 - 1; j++) {
+//	for (int i = 0; i < report_len; i++) { // report_cpy에 요소가 중첩인 경우 중첩인 요소 null로 변경
+//		for (int j = 0; j < report_len - 1; j++) {
 //			if (report[i] == report[j + 1])
 //				strcpy(report_cpy[j + 1], NULL);
 //		}
 //	}
 //
+//	for (int i = 0; i < id_list_len; i++)
+//	{
+//		if (strchr(report_cpy[i], ' ') != NULL) {
+//			
+//		}
+//
+//		for (int j = 0; j < id_list_len - 1; j++)
+//		{
+//			if (strcmp(report[i], id_list[j]) == 0)
+//				count++;
+//		}			
+//
+//	}
+//
+//
+//
+//	return answer;
+//}
+
+//int main() {
+//	char id_list[][11] = { "muzi","frodo","apeach","neo" };
+//	char report[][21] = { "muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi","frode neo","muzi frodo" };
+//	char cpy_report[7][21] = { 0, };
+//	char attack[7][11] = { 0, };
+//	char attacked[7][11] = { 0, };
+//	char* ptr ;
+//	int k = 0;
+//	int i = 0, j=0 ;
+//
+//	strcpy(cpy_report, report);
+//
+//	for (i = 0; i < 7; i++) {
+//
+//		ptr = strtok(report[i], " ");
+//		strcpy(attack[i], ptr);
+//
+//		ptr = strtok(NULL, " ");
+//		strcpy(attacked[i], ptr);
+//
+//		
+//	}
+//
+//	for (i = 0; i < 7; i++) {
+//		for (j = 0; j < 7-i; j++) {
+//			if (strcmp(report[i], report[j + 1]) == 0 ) {
+//				strcpy(cpy_report[i], "0");
+//			}
+//		}
+//	}
+//
+//
+//	for (i = 0; i < 7; i++) {
+//		printf("%s\n", cpy_report[i]);
+//	}
+//
+	
+	
+
+
+	//while (report[i] != NULL) {
+	//	if (strcmp(report[i], report[i + 1]) == 0)
+	//		strcpy(report[i], NULL);
+	//	i++;
+	//}
+
+	//for (int i = 0; i<5; i++)
+	//{
+	//	printf("%s\n", report[i]);
+	//}
+	//
+
+
+
+	//char* report_cpy[] = { 0, };
+
+	//for (int i = 0; i < 5; i++) {
+	//	strcpy(report_cpy[i], report[i]);
+	//}
+
+	//for (int i = 0; i < 5; i++) {
+	//	printf("%s\n", report_cpy[i]);
+	//}
+
+	//int i = 0;
+
+	//for (i = 0; i < 5; i++) {
+	//	for (int j = 0; j < 5 - 1; j++) {
+	//		if (report[i] == report[j + 1])
+	//			strcpy(report_cpy[j + 1], NULL);
+	//	}
+	//}
+
 //}
 
 //void main()
@@ -189,14 +240,93 @@ int* solution(const char* id_list[], size_t id_list_len, const char* report[], s
 //}
 
 
-/*특정문자가 있을경우 포인터 이동 하는 법 파악*/
-int main() {
-	char* str = "Hello Wolrd";
+///*특정문자가 있을경우 포인터 이동 하는 법 파악*/
+//int main() {
+//	char* str = "Hello Wolrd";
+//
+//	printf("%p -------- %s \n", str, str);
+//
+//	str = strchr(str, ' ');
+//	str++;
+//
+//	printf("%p ------------%s\n", str, str);
+//}
 
-	printf("%p -------- %s \n", str, str);
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
 
-	str = strchr(str, ' ');
-	str++;
-
-	printf("%p ------------%s\n", str, str);
-}
+//// id_list_len은 배열 id_list의 길이입니다.
+//// report_len은 배열 report의 길이입니다.
+//// 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
+//int* solution(const char* id_list[], size_t id_list_len, const char* report[], size_t report_len, int k) {
+//	// return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
+//
+//	int temp[id_list_len][id_list_len];
+//
+//	for (int i = 0; i < id_list_len; ++i)
+//	{
+//		for (int j = 0; j < id_list_len; ++j)
+//		{
+//			temp[i][i] = 0;
+//		}
+//	}
+//
+//
+//	for (int i = 0; i < report_len; ++i)
+//	{
+//		char* report_edit;
+//		report_edit = strtok(report[i], " ");
+//
+//		int idx1 = 0;
+//		for (int j = 0; j < id_list_len; ++j)
+//		{
+//			if (strcmp(id_list[j], report_edit) == 0)
+//			{
+//				idx1 = j;
+//			}
+//		}
+//
+//		report_edit = strtok(NULL, " ");
+//		int idx2 = 0;
+//		for (int j = 0; j < id_list_len; ++j)
+//		{
+//			if (strcmp(id_list[j], report_edit) == 0)
+//			{
+//				idx2 = j;
+//			}
+//		}
+//		temp[idx1][idx2] = 1;
+//	}
+//
+//	int result[id_list_len];
+//	for (int j = 0; j < id_list_len; ++j)
+//	{
+//		result[j] = 0;
+//		for (int i = 0; i < id_list_len; ++i)
+//		{
+//			if (temp[i][j] == 1)
+//			{
+//				result[j] += 1;
+//			}
+//		}
+//	}
+//
+//
+//	int* answer = (int*)malloc(id_list_len * sizeof(int));
+//
+//	for (int j = 0; j < id_list_len; ++j)
+//	{
+//		answer[j] = 0;
+//		for (int i = 0; i < id_list_len; ++i)
+//		{
+//			if (temp[j][i] == 1 && result[i] >= k)
+//			{
+//				answer[j] += 1;
+//			}
+//		}
+//	}
+//
+//
+//	return answer;
+//}
