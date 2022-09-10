2cb7ffcd69c277197fe4809fec517ce3a564bf5a
YoonSeung <46475661+YoonSeung@users.noreply.github.com>
2022-09-10 19:01:22 +0900

    링크드리스트

--- HonGong/linkedlist.c
+++ HonGong/linkedlist.c
@@ -1,17 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct {
-	int value;
-	struct Node *next;
-} Node;
+struct NODE {
+	int data;
+	struct NODE *next;
+};
 
 
 void main() {
 
-	struct Node *head = (Node*)malloc(sizeof(Node));
+	struct NODE *head = malloc(sizeof(struct NODE));
 
+	struct NODE *node1 = malloc(sizeof(struct NODE));
+	head->next = node1;
+	node1->data = 10;
 
+	struct NODE *node2 = malloc(sizeof(struct NODE));
+	node1->next = node2;
+	node2->data = 20;
+
+	node2->next = NULL;
+	// 뒤에 부분 더 공부해야함
+	//출력하는것, 삽입,삭제, 표현 
+	//원형연결리스트
+	//이중연결리스트
 }
 
 
